3c4f1b853bb920b6b7fd65cdb8627e6cd862d2f7
skins346 <infall346@gmail.com>
2017-05-02 17:46:19 +0900

    2017-05-02
    2017-05-02

---
+++ 완전탐색0/이전순열(완전탐색0).cpp
@@ -0,0 +1,28 @@
+////2017-05-02
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int N; 
+//int main()
+//{
+//	scanf("%d",& N);
+//	vector<int> v(N);
+//
+//	for (int i = 0; i < N; i++)
+//		scanf("%d", &v[i]);
+//
+//	if (prev_permutation(v.begin(), v.end())){
+//		for (int i = 0; i < N; i++)
+//			printf("%d ", v[i]);
+//	}
+//	else
+//		printf("-1");
+//
+//		
+//	return 1;
+//}
+//
